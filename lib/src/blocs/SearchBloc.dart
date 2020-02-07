5908bf90cf96e618ef73f468978e08ad0f3ab870
Gabriel Melo <carlos.gabriel96@gmail.com>
2020-02-07 19:29:24 -0300

    Project structure improvements

--- lib/src/blocs/SearchBloc.dart
+++ lib/src/blocs/SearchBloc.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
+import 'package:github_search/src/models/SearchItem.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:github_search/src/models/SearchResult.dart';
 import 'package:github_search/src/services/data/GithubService.dart';
 
 class SearchBloc {
@@ -10,15 +10,16 @@ class SearchBloc {
   Stream<String> get searchFlux => _searchController.stream;
   Sink<String> get searchEvent => _searchController.sink;
 
-  Stream<SearchResult> apiResultFlux;
+  Stream<List<SearchItem>> apiResultFlux;
 
   SearchBloc(){
     apiResultFlux = searchFlux
         .distinct()
-        .where((valor) => valor.length>2)
+        .where((response) => response.length>2)
         .debounceTime(Duration(milliseconds: 500))
         .asyncMap(_service.search)
-        .switchMap((valor) => Stream.value(valor));
+        .switchMap((response) => Stream.value(response.items));
+
   }
 
   void dispose(){
--- lib/src/services/data/GithubService.dart
+++ lib/src/services/data/GithubService.dart
@@ -1,18 +1,38 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:github_search/src/models/SearchResult.dart';
 
-class GithubService{
-  final Dio dio = new Dio();
+class GithubService {
+  final Dio _dio = Dio();
+
+  GithubService(){
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (RequestOptions options) async {
+        log("requisição foi interceptada antes de ser feita");
+        options.baseUrl = "https://api.github.com/";
+        return options;
+      },
+      onResponse: (Response response){
+        log("requisição foi interceptadas após resposta");
+        return response;
+      },
+      onError: (DioError error) async {
+        log("requisição com erro foi interceptada");
+        return error;
+      }
+    ));
+  }
 
   Future<SearchResult> search(String term) async {
     try{
-      Response response = await dio.get("https://api.github.com/search/repositories?q="+term);
+      Response response = await _dio.get("search/repositories?q="+term);
 
       return SearchResult.fromJson(response.data);
-    }catch (e) {
-      throw SocketException(e);
+    }catch (error){
+      throw SocketException(error.message);
     }
   }
+
 }
